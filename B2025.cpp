b7e275f21af3a0c8f6b75f54466b890faa94a529
ritenseki <ritenseki@proton.me>
2023-02-20 22:00:30 +0800

    solve B2025 and renamed others

---
+++ B2025.cpp
@@ -0,0 +1,28 @@
+/*
+  *
+ ***
+*****
+ ***
+  *
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void line1(){
+    cout<<"  *"<<endl;
+}
+void line2(){
+    cout<<" ***"<<endl;
+}
+void line3(){
+    cout<<"*****"<<endl;
+}
+
+int main(){
+    line1();
+    line2();
+    line3();
+    line2();
+    line1();
+    return 0;
+}
